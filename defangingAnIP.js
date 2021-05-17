7a596dd2e03ef576788cc02412238af0ecd67a60
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-05-17 08:41:07 -0400

    Revised defanging an IP with comments and changes to test case

--- defangingAnIP.js
+++ defangingAnIP.js
@@ -5,6 +5,14 @@
  * @return {string}
  */
  const defangIPaddr = (address) => {
-  // join on defanger
+  // One liner
+  // split on the dot
+  // Join with our new separator
   return address.split('.').join('[.]');
 };
+
+var ipAddresses = ["1.1.1.1", '1.0.0.1', '8.8.8.8', '192.168.1.105'];
+
+for (let i = 0; i < ipAddresses.length; i += 1) {
+  console.log(defangIPaddr(ipAddresses[i]));
+}
